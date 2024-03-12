5c16fe65c6e9c5f22f30c156e24abbb4bb96529c
Selyan Kablia <selyan.kablia@ens.uvsq.fr>
2024-03-12 14:16:54 +0100

    maj eval

--- evaluator.c
+++ evaluator.c
@@ -10,30 +10,56 @@
 // Fonction pour évaluer une expression
 double evaluateExpression(Token* tokens) {
     Stack* stack = createStack(100);
+    double lastNumber = 0.0;
+    bool hasLastNumber = false;
 
     for (int i = 0; tokens[i].type != TOKEN_EOF; ++i) {
         switch (tokens[i].type) {
             case TOKEN_NUMBER:
-                push(stack, tokens[i].value);
+                lastNumber = tokens[i].value;
+                printf("lastnumb %f ", lastNumber);
+                hasLastNumber = true;
                 break;
-            case TOKEN_PLUS: {
-                double operand2 = pop(stack);
-                double operand1 = pop(stack);
-                push(stack, operand1 + operand2);
+            case TOKEN_PLUS:
+                if (hasLastNumber) {
+                    push(stack, lastNumber);
+                    hasLastNumber = false;
+                }
+                // Pas besoin de faire quoi que ce soit pour l'addition, elle sera effectuée au prochain tour de boucle
                 break;
-            }
-            case TOKEN_MULTIPLY: {
-                double operand2 = pop(stack);
-                double operand1 = pop(stack);
-                push(stack, operand1 * operand2);
+            case TOKEN_MULTIPLY:
+                if (hasLastNumber) {
+                    push(stack, lastNumber);
+                    hasLastNumber = false;
+                }
+                // Pas besoin de faire quoi que ce soit pour la multiplication, elle sera effectuée au prochain tour de boucle
                 break;
-            }
             default:
                 // Ignorer les autres types de token
                 break;
         }
+
+        // Si on a rencontré un opérateur, et qu'il y a déjà un dernier nombre dans la pile, effectuer l'opération
+        if (!hasLastNumber && (tokens[i].type == TOKEN_PLUS || tokens[i].type == TOKEN_MULTIPLY)) {
+            double operand1 = pop(stack);
+            double operand2 = pop(stack);
+
+            printf("op1 %f op2 %f ", operand1, operand2);
+            printf("lastNumber: %f\n", lastNumber);
+            switch (tokens[i].type) {
+                case TOKEN_PLUS:
+                    push(stack, operand1 + operand2);
+                    break;
+                case TOKEN_MULTIPLY:
+                    push(stack, operand1 * operand2);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
+    // Retourner le résultat final
     double result = pop(stack);
     free(stack->data);
     free(stack);
